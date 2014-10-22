86cde12f7ed79b3d7586fbb0def206da816ac1b8
Mano Marks <mmarks@google.com>
2014-10-22 11:30:05 -0700

    fixing formatting and some bugs in core.js

--- controllers/core.js
+++ controllers/core.js
@@ -3,7 +3,6 @@ var jsonp = require('../helpers/jsonp');
 var mysql2geojson = require('mysql2geojson');
 var mysql = require('mysql');
 
-
 var pool = mysql.createPool({
   host: 'YourHost',
   user: 'UserName',
@@ -11,43 +10,47 @@ var pool = mysql.createPool({
   database: 'Countries'
 });
 
-module.exports.controller = function(app) { // Queries shapes interesections against the countries table return GeoJSON.
-  app.get('/countries/intersect', function(req, res) {
-      pool.getConnection(function(err, connection) {
+module.exports.controller = function (app) {
+  // Queries shapes interesections against the countries table return GeoJSON.
+  app.get('/countries/intersect', function (req, res) {
+    pool.getConnection(function (err, connection) {
       var path = req.query.path;
       var type = req.query.type.toUpperCase();
-
-      if (type == 'POLYLINE') type = 'LINESTRING';
-      if (type == 'POINT') path = req.query.lnglat;
+      if (type === 'POLYLINE') {
+        type = 'LINESTRING';
+      }
+      if (type === 'POINT') {
+        path = req.query.lnglat;
+      }
 
       // Set-up query.
       connection.query('SELECT name_long, pop_est, AsWKT(SHAPE) as ' +
         'geometry FROM countries where st_intersects(SHAPE,GeomFromText(' +
         '"' + type + '((' + path + '))")) and pop_est > 4000;',
         handleResult(res, connection, error, result)
-      );
-    }
-  );
-}):
-
-  app.get('/countries/all', function(req, res) {
-    pool.getConnection(function(err, connection) {
+        );
+    });
+  });
+  app.get('/countries/all', function (req, res) {
+    pool.getConnection(function (err, connection) {
       // Set-up query.
       connection.query('SELECT name_long, pop_est, AsWKT(SHAPE) as geometry' +
         ' FROM countries where pop_est > 4000;',
         handleResult(res, connection, error, result)
-      );
+        );
     }
-  );
-}):
-
-function handleResult(res, connection, error, result) {
-  connection.release();
-  mysql2geojson.parse({
-  data: rows,
-  format: 'geojson',
-  callback: function(error, result) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(error || result);
+      );
   });
-}
+
+  function handleResult(res, connection, error, result) {
+    connection.release();
+    mysql2geojson.parse({
+      data: rows,
+      format: 'geojson',
+      callback: function (error, result) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(error || result);
+      }
+    });
+  }
+};

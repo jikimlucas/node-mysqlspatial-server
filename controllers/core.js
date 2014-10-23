d1aa559acbe2248904f81dcf535de4e6639e9b15
Mano Marks <mano.marks@gmail.com>
2014-10-23 01:01:34 +0000

    reformatting in drawing.js, and some refactoring. Improving core.js

--- controllers/core.js
+++ controllers/core.js
@@ -4,9 +4,9 @@ var mysql2geojson = require('mysql2geojson');
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-  host: 'YourHost',
-  user: 'UserName',
-  password: 'YourPassword',
+  host: '173.194.251.67',
+  user: 'root',
+  password: 'rootMano',
   database: 'Countries'
 });
 
@@ -14,44 +14,44 @@ module.exports.controller = function(app) {
   // Queries shapes interesections against the countries table return GeoJSON.
   app.get('/countries/intersect', function(req, res) {
     pool.getConnection(function(err, connection) {
-      var path = req.query.path;
       var type = req.query.type.toUpperCase();
-      if (type === 'POLYLINE') {
-        type = 'LINESTRING';
-      }
-      if (type === 'POINT') {
-        path = req.query.lnglat;
-      }
-
       // Set-up query.
-      connection.query('SELECT name_long, pop_est, AsWKT(SHAPE) as ' +
+      var connstr;
+      if (type === 'ALL') {
+        connstr = 'SELECT name_long, pop_est, AsWKT(SHAPE) as geometry' +
+        ' FROM countries where pop_est > 4000;';
+      } else {
+        var path = req.query.path;
+        var pathString;
+        if (type === 'POLYLINE') {
+          pathString = connection.escape('LINESTRING(' + path + ')') + '))' +
+          ' and pop_est > 4000;';
+        }
+        if (type === 'POINT') {
+          pathString = connection.escape('POINT(' + req.query.lnglat +
+          ')') + ')) and pop_est > 4000;';
+        }
+        if (type === 'POLYGON') {
+          pathString = connection.escape('POLYGON((' + path + '))') + '))' +
+          ' and pop_est > 4000;';
+        }
+        connstr = 'SELECT name_long, pop_est, AsWKT(SHAPE) as ' +
         'geometry FROM countries where st_intersects(SHAPE,GeomFromText(' +
-        '"' + type + '((' + path + '))")) and pop_est > 4000;',
-        handleResult(res, connection, err, result)
-        );
-    });
-  });
-  app.get('/countries/all', function(req, res) {
-    pool.getConnection(function(err, connection) {
-      // Set-up query.
-      connection.query('SELECT name_long, pop_est, AsWKT(SHAPE) as geometry' +
-        ' FROM countries where pop_est > 4000;',
-        handleResult(res, connection, err, result)
-        );
-    }
-      );
-  });
-
-  function handleResult(res, connection, err, result) {
-    connection.release();
-    mysql2geojson.parse({
-      data: rows,
-      format: 'geojson',
-      callback: function(err, result) {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(err || result);
+        pathString;
       }
-    });
-  }
-};
 
+      // Send off query and return result.
+      connection.query(connstr, function(err, rows) {
+        mysql2geojson.parse({
+        data: rows,
+        format: 'geojson',
+        callback: function(error, result) {
+          connection.release();
+          res.setHeader('Content-Type', 'application/json');
+          res.send(err || result);
+        }
+      });
+    });
+  });
+});
+};
--- drawing.js
+++ drawing.js
@@ -2,6 +2,8 @@
  * @fileoverview Description of this file.
  */
 
+// Indicates whether to display the info box which shows the population of a
+// feature.
 var ibDisplay = false;
 function initialize() {
   var mapOptions = {
@@ -9,7 +11,7 @@ function initialize() {
     zoom: 3
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+      mapOptions);
   var drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: google.maps.drawing.OverlayType.POLYGON,
     drawingControl: true,
@@ -26,61 +28,60 @@ function initialize() {
   drawingManager.setMap(map);
   google.maps.event.addListener(drawingManager, 'overlaycomplete',
       function(event) {
+        google.maps.event.clearListeners(map.data, 'addfeature')
         map.data.setMap(null);
         map.data = new google.maps.Data({map: map});
         map.data.addListener('addfeature', function(event) {
-        updateInfoBox(event);
-  });
+          updateInfoBox(event);
+        });
   map.data.addListener('mouseover', function(event) {
-  updateInfoBox(event);
+    updateInfoBox(event);
   });
-      switch (event.type) {
-      case 'polygon':
-        var pathArray = event.overlay.getPath().getArray();
-        pathArray.push(pathArray[0]);
-        var stringArray = [];
-        pathArray.forEach(function(element, index) {
-          stringArray.push(element.lng() + ' ' + element.lat());
-        });
-        var pathString = stringArray.join(',');
-        map.data.loadGeoJson('/countries/intersect?type=polygon&path=' +
-            pathString);
-        event.overlay.setMap(null);
-        break;
-      case 'rectangle':
-        var bounds = event.overlay.getBounds();
-        var ne = bounds.getNorthEast();
-        var sw = bounds.getSouthWest();
-        var stringArray = [];
-        stringArray.push(ne.lng() + ' ' + ne.lat());
-        stringArray.push(ne.lng() + ' ' + sw.lat());
-        stringArray.push(sw.lng() + ' ' + sw.lat());
-        stringArray.push(sw.lng() + ' ' + ne.lat());
-        stringArray.push(ne.lng() + ' ' + ne.lat());
-        var pathString = stringArray.join(',');
-        map.data.loadGeoJson('/countries/intersect?type=polygon&path=' +
-            pathString);
-        event.overlay.setMap(null);
-        break;
-      case 'polyline':
-        var pathArray = event.overlay.getPath().getArray();
-        var stringArray = [];
-        pathArray.forEach(function(element, index) {
-          stringArray.push(element.lng() + ' ' + element.lat());
-        });
-        var pathString = stringArray.join(',');
-        map.data.loadGeoJson('/countries/intersect?type=polyline&path=' +
-            pathString);
-        event.overlay.setMap(null);
-        break;
-      case 'marker':
-        var latlng = event.overlay.getPosition();
-        var lnglat = latlng.lng() + ' ' + latlng.lat();
-        map.data.loadGeoJson('/countries/intersect?type=point&lnglat=' +
-            lnglat);
-        event.overlay.setMap(null);
-        break;
-    }
+
+  event.overlay.setMap(null);    
+  switch (event.type) {
+    case 'polygon':
+      var pathArray = event.overlay.getPath().getArray();
+      pathArray.push(pathArray[0]);
+      var stringArray = [];
+      pathArray.forEach(function(element, index) {
+        stringArray.push(element.lng() + ' ' + element.lat());
+      });
+      var pathString = stringArray.join(',');
+      map.data.loadGeoJson('/countries/intersect?type=polygon&path=' +
+          pathString);
+      break;
+    case 'rectangle':
+      var bounds = event.overlay.getBounds();
+      var ne = bounds.getNorthEast();
+      var sw = bounds.getSouthWest();
+      var stringArray = [];
+      stringArray.push(ne.lng() + ' ' + ne.lat());
+      stringArray.push(ne.lng() + ' ' + sw.lat());
+      stringArray.push(sw.lng() + ' ' + sw.lat());
+      stringArray.push(sw.lng() + ' ' + ne.lat());
+      stringArray.push(ne.lng() + ' ' + ne.lat());
+      var pathString = stringArray.join(',');
+      map.data.loadGeoJson('/countries/intersect?type=polygon&path=' +
+          pathString);
+      break;
+    case 'polyline':
+      var pathArray = event.overlay.getPath().getArray();
+      var stringArray = [];
+      pathArray.forEach(function(element, index) {
+        stringArray.push(element.lng() + ' ' + element.lat());
+      });
+      var pathString = stringArray.join(',');
+      map.data.loadGeoJson('/countries/intersect?type=polyline&path=' +
+          pathString);
+      break;
+    case 'marker':
+      var latlng = event.overlay.getPosition();
+      var lnglat = latlng.lng() + ' ' + latlng.lat();
+      map.data.loadGeoJson('/countries/intersect?type=point&lnglat=' +
+          lnglat);
+      break;
+  }
 });
 }
 function updateInfoBox(event) {
@@ -90,7 +91,7 @@ function updateInfoBox(event) {
     }
     document.getElementById('info-box').textContent =
         event.feature.getProperty('name_long') + ' estimated population: ' +
-        formatNumber(Math.round(event.feature.getProperty('pop_est'), 0));
+        (Math.round(event.feature.getProperty('pop_est'), 0)).toLocaleString();
 }
 function formatNumber(n) {
   return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
